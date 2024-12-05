72970b3e39f6e5cb8bd80112510dd2c7fee87991
symmetrysorrow <92022965+symmetrysorrow@users.noreply.github.com>
2024-12-05 12:29:27 +0900

    0.02

--- tagawa_select.py
+++ tagawa_select.py
@@ -2,8 +2,8 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 # ファイルの読み込み
-file_path_0 = "E:/tagawa/20241203/room1_ch1ch2_220mK_920uA920uA_gain5_trig0.2V_rate500k_samples100k/CH0_pulse/output0.csv"  # output0のファイル名
-file_path_1 = "E:/tagawa/20241203/room1_ch1ch2_220mK_920uA920uA_gain5_trig0.2V_rate500k_samples100k/CH1_pulse/output1.csv"  # output1のファイル名
+file_path_0 = "F:/tagawa/data/20241203/room1_ch1ch2_220mK_920uA920uA_gain5_trig0.2V_rate500k_samples100k/CH0_pulse/output0.csv"  # output0のファイル名
+file_path_1 = "F:/tagawa/data/20241203/room1_ch1ch2_220mK_920uA920uA_gain5_trig0.2V_rate500k_samples100k/CH1_pulse/output1.csv"  # output1のファイル名
 
 # 各ファイルをDataFrameに読み込む
 df0 = pd.read_csv(file_path_0)
@@ -25,20 +25,14 @@ if base_0 is not None and base_1 is not None:
     
     # プロット
     plt.figure(figsize=(8, 8))
-    plt.scatter(height_base_0, height_base_1, alpha=0.7)
-    plt.title('Scatter Plot: (height - base) for output0 vs output1')
-    plt.xlabel('output0 - (height - base)')
-    plt.ylabel('output1 - (height - base)')
+    plt.scatter(height_0, height_1, alpha=0.7)
+    plt.title('Scatter Plot: (height) for output0 vs output1')
+    plt.xlabel('output0 - (height0)')
+    plt.ylabel('output1 - (height1)')
     plt.grid(True)
     plt.show()
 
-    plt.figure(figsize=(8, 8))
-    plt.scatter(rise_0, height_0, alpha=0.7)
-    plt.title('Scatter Plot: (height - base) for output0 vs output1')
-    plt.xlabel('output0 - (height - base)')
-    plt.ylabel('output1 - (height - base)')
-    plt.grid(True)
-    plt.show()
+
 
 else:
     print("いずれかのファイルに 'base' 列が存在しませんでした。")
